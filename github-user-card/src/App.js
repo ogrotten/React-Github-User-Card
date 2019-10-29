35810ee83c08b8df68a461c5a88c65b090322157
Darrin Lowery <lambda@monsto.com>
2019-10-29 14:53:08 -0500

    setup axios

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./App.css";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function clg(...x) {
+	for (let exes of x) console.log(exes);
+}
+
+class App extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			user: {}
+		};
+	}
+
+	componentDidMount() {
+		clg(">>> Mount");
+		axios
+			.get("https://api.github.com/users/ogrotten")
+			.then(res => {
+				this.setState({user: res.data}) 
+				clg(this.state.user);
+			})
+			.catch(err => {
+				clg(`Problem: ${err}`);
+			});
+	}
+
+	componentDidUpdate() {
+		clg(">>> UpD");
+	}
+
+	render() {
+		return (
+			<div className="App">
+				MEH
+			</div>
+		)
+	}
 }
 
 export default App;
