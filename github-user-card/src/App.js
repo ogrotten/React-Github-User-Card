53fe6ffd1cd4bc0abaa39aee9056beec8345f1df
Darrin Lowery <lambda@monsto.com>
2019-10-29 15:20:43 -0500

    setup user component

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./App.css";
 import axios from "axios";
+import User from "./User";
 
 function clg(...x) {
 	for (let exes of x) console.log(exes);
@@ -36,6 +37,7 @@ class App extends React.Component {
 		return (
 			<div className="App">
 				MEH
+				<User followers={this.state.user.followers}></User>
 			</div>
 		)
 	}
---
+++ github-user-card/src/User.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
+import styled from "styled-components";
+
+export default function User(props) {
+
+
+	return (
+		<div>{props.followers}</div>
+	)
+}
